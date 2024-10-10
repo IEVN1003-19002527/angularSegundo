36f7e9b4f9b565925594c3fb8702a929e8978cab
OMC-13 <19002527@alumnos.utleon.edu.mx>
2024-10-09 18:59:57 -0600

    primer commit

---
+++ src/app/formularios/ejemplo1/ejemplo1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejemplo1',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejemplo1.component.html',
+  styleUrls: ['./ejemplo1.component.css'] // Cambiado a styleUrls
+})
+export class Ejemplo1Component {
+  // Puedes agregar lógica aquí si es necesario
+}
