5baffa23269de0c0a4fc39cb79ba11f3cd1dcee1
OMC-13 <19002527@alumnos.utleon.edu.mx>
2024-10-15 19:19:31 -0600

    RUTAS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,3 +13,18 @@ import { ZodiacoComponent } from "./formularios/zodiaco/zodiaco.component";
 export class AppComponent {
   title = 'angularSegundo';
 }
+
+onsubmit(): void{
+
+  localStorage.setItem('nombre',this.nombre)
+  const{nombre, edad, email}=this.formGroup.value;
+
+  this.persona.nombre=nombre;
+  this.persona.edad=edad;
+  this.persona.email=email;
+
+  let personaJSON=JSON.stringify(this.persona);
+  localStorage.setItem('persona', personaJSON);
+
+  //console.log(this.persona);
+}
